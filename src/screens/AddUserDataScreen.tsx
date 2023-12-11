0d273bdd93fed467c879f4eba7d7e39fd9f09640
Sanket V <sanketvithani6@gmail.com>
2023-12-11 11:32:38 +0530

    => Add Google ads => Add patch-package

--- src/screens/AddUserDataScreen.tsx
+++ src/screens/AddUserDataScreen.tsx
@@ -1,11 +1,20 @@
-import React, {useState} from 'react';
 import {useDispatch} from 'react-redux';
 import {StyleSheet, View} from 'react-native';
 import {userData} from '../redux/actios/Action';
+import React, {useEffect, useState} from 'react';
 import ButtonComp from '../components/ButtonComp';
 import InputTextComp from '../components/InputTextComp';
 import {DealerNavigNavigationType} from '../navigation';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {
+  TestIds,
+  BannerAd,
+  RewardedAd,
+  AdEventType,
+  BannerAdSize,
+  InterstitialAd,
+  RewardedAdEventType,
+} from 'react-native-google-mobile-ads';
 
 type AddUserDataScreen = NativeStackScreenProps<
   DealerNavigNavigationType,
@@ -14,12 +23,67 @@ type AddUserDataScreen = NativeStackScreenProps<
   navigation: () => void;
 };
 
+const adUnitId = __DEV__
+  ? TestIds.ADAPTIVE_BANNER
+  : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy';
+
+const InterstitialAdID = __DEV__
+  ? TestIds.INTERSTITIAL
+  : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy';
+
+const interstitial = InterstitialAd.createForAdRequest(InterstitialAdID, {
+  keywords: ['fashion', 'clothing'],
+});
+
+const RewardedAdID = __DEV__
+  ? TestIds.REWARDED
+  : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy';
+
+const rewarded = RewardedAd.createForAdRequest(RewardedAdID, {
+  keywords: ['fashion', 'clothing'],
+});
+
 const AddUserDataScreen: React.FC<AddUserDataScreen> = ({navigation}) => {
+  const dispatch = useDispatch();
   const [age, setAge] = useState<string | number>('');
   const [city, setCity] = useState<string | number>('');
   const [name, setName] = useState<string | number>('');
+  const [interstitialLoaded, setInterstitialLoaded] = useState(false);
+  const [rewardedLoaded, setRewardedLoaded] = useState(false);
 
-  const dispatch = useDispatch();
+  useEffect(() => {
+    const unsubscribe = interstitial.addAdEventListener(
+      AdEventType.LOADED,
+      () => {
+        setInterstitialLoaded(true);
+      },
+    );
+    const unsubscribeLoaded = rewarded.addAdEventListener(
+      RewardedAdEventType.LOADED,
+      () => {
+        setRewardedLoaded(true);
+      },
+    );
+    const unsubscribeEarned = rewarded.addAdEventListener(
+      RewardedAdEventType.EARNED_REWARD,
+      reward => {},
+    );
+    rewarded.load();
+    interstitial.load();
+    return () => {
+      unsubscribeLoaded();
+      unsubscribeEarned();
+      unsubscribe();
+    };
+  }, []);
+
+  if (!rewardedLoaded) {
+    return null;
+  }
+
+  if (!interstitialLoaded) {
+    return null;
+  }
 
   const onAddPress = () => {
     let userDetails = {
@@ -35,6 +99,10 @@ const AddUserDataScreen: React.FC<AddUserDataScreen> = ({navigation}) => {
 
   return (
     <View style={styles.container}>
+      <BannerAd
+        unitId={adUnitId as string}
+        size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+      />
       <InputTextComp
         value={name}
         placeholder="Enter Name"
@@ -62,6 +130,22 @@ const AddUserDataScreen: React.FC<AddUserDataScreen> = ({navigation}) => {
             navigation.navigate('GetUserData');
           }}
           title="Go to next screen"
+        />
+      </View>
+      <View style={styles.buttonView}>
+        <ButtonComp
+          onPress={() => {
+            interstitial.show();
+          }}
+          title="Show Interstitial"
+        />
+      </View>
+      <View style={styles.buttonView}>
+        <ButtonComp
+          onPress={() => {
+            rewarded.show();
+          }}
+          title="Show Rewarded Ad"
         />
       </View>
     </View>
